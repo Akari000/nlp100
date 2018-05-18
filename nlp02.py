c4ad145fb04e66247a3ca6fff8419ad242d4d62c
Akari000 <app.sub.k@gmail.com>
2018-05-18 16:35:33 +0900

    add new file

---
+++ nlp02.py
@@ -0,0 +1,11 @@
+
+# -*- coding: utf-8 -*-
+str = u"パタトクカシーー"
+newStr = ""
+
+for i in range(0,len(u"パタトクカシーー"),2):
+    print(i)
+    newStr += str[i+1]
+    print(str[i].encode('utf-8'))
+
+print(newStr.encode('utf-8'))
