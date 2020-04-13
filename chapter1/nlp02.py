685540d711590d9cd3eab073d84e5cc2a0b33720
Akari000 <app.sub.k@gmail.com>
2020-04-13 11:12:20 +0900

    fix 03

--- chapter1/nlp02.py
+++ chapter1/nlp02.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 text1 = u"パトカー"
 text2 = u"タクシー"
-result = ""
 
 for t1, t2 in zip(text1, text2):
     print(t1 + t2, end="")
--- chapter1/nlp03.py
+++ chapter1/nlp03.py
@@ -1,4 +1,6 @@
+import re
 text = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."
+text = re.sub(r"[,.]", "", text)
 text = text.split(" ")
 
 count = [len(word) for word in text]
