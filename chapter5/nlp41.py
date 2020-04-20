f6fb57e7ec7bc9889d2678646658c38f6ede162c
Akari000 <app.sub.k@gmail.com>
2020-04-20 22:12:36 +0900

    add 41

--- chapter5/nlp41.py
+++ chapter5/nlp41.py
@@ -4,6 +4,8 @@
 # さらに，入力テキストのCaboChaの解析結果を読み込み，１文をChunkオブジェクトのリストとして表現し，8文目の文節の文字列と係り先を表示せよ．
 # 第5章の残りの問題では，ここで作ったプログラムを活用せよ．
 
+import re
+
 
 class Morph(object):
     def __init__(self, surface, base, pos, pos1):
@@ -20,21 +22,59 @@ class Chunk(object):
         self.srcs = srcs
 
 
-def get_morphs(text):
-    morphs = []
-    for sentence in text.split('EOS\n')[:6]:
-        if len(sentence) < 1:
-            continue
-        for line in sentence.split('\n')[:-1]:  # 空文字を除く
-            if line[0] == '*':
-                continue
-            surface = line.split('\t')[0]
-            line = line.split('\t')[1].split(',')
-            morph = Morph(surface, line[6], line[0], line[1])
-            morphs.append(morph)
-    return morphs
+def get_chunks(text):
+    data = []
+    for sentence in re.findall(r'(\*[\s\S]*?EOS)', text):
+        chunks = []
+        for clause in re.findall(
+                r'\* (\d*) (-?\d+\w+).*?\n([\s\S]*?)(?=\n\*|\nEOS)', sentence):  #(srcs) (dst) (morphs)
+            morphs = []
+            for line in re.findall(r'(.*?)\t(.*?)(?:$|\n)', clause[2]):  # tab前から行末または\nまで
+                surface = line[0]
+                feature = line[1].split(',')
+                morph = Morph(surface, feature[0], feature[1], feature[6])
+                morphs.append(morph)
+            chunk = Chunk(morphs, clause[1], clause[0])
+            chunks.append(chunk)
+        data.append(chunks)
+    return data
 
 
-with open('../data/neko.txt.cabocha', 'r') as f:
-    text = f.read()
-morphs = get_morphs(text)
+if __name__ == "__main__":
+    with open('../data/neko.txt.cabocha', 'r') as f:
+        text = f.read()
+        data = get_chunks(text)
+        for chunk in data[7]:
+            print('dst', chunk.dst, 'srcs', chunk.srcs)
+            for morph in chunk.morphs:
+                print(morph.surface, morph.base, morph.pos, morph.pos1)
+
+
+'''
+dst 1D srcs 0
+この 連体詞 * この
+dst 7D srcs 1
+書生 名詞 一般 書生
+という 助詞 格助詞 という
+の 名詞 非自立 の
+は 助詞 係助詞 は
+dst 4D srcs 2
+時々 副詞 一般 時々
+dst 4D srcs 3
+我々 名詞 代名詞 我々
+を 助詞 格助詞 を
+dst 5D srcs 4
+捕え 動詞 自立 捕える
+て 助詞 接続助詞 て
+dst 6D srcs 5
+煮 動詞 自立 煮る
+て 助詞 接続助詞 て
+dst 7D srcs 6
+食う 動詞 自立 食う
+という 助詞 格助詞 という
+dst -1D srcs 7
+話 名詞 サ変接続 話
+で 助動詞 * だ
+ある 助動詞 * ある
+。 記号 句点 。
+'''
