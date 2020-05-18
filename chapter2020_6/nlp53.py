86f4cc1c83c7cbdb29aa6c2021ac3ef934e821ff
Akari000 <app.sub.k@gmail.com>
2020-05-18 15:49:19 +0900

    add probability

--- chapter2020_6/nlp53.py
+++ chapter2020_6/nlp53.py
@@ -15,16 +15,14 @@ test = pd.read_csv('../data/NewsAggregatorDataset/test.txt',
 
 label2int = {'b': 0, 't': 1, 'e': 2, 'm': 3}
 y_train = train['category'].map(label2int)  # クラスを定義
-y_test = test['category'].map(label2int)
-
 x_train = pd.read_csv('../data/NewsAggregatorDataset/train.feature.txt',
                       sep='\t', header=None)
-x_test = pd.read_csv('../data/NewsAggregatorDataset/test.feature.txt',
-                     sep='\t', header=None)
 
 # 学習
 lr = LogisticRegression(class_weight='balanced')  # ロジスティック回帰モデルのインスタンスを作成
 lr.fit(x_train, y_train)  # ロジスティック回帰モデルの重みを学習
+
+# TODO 予測確率を出す
 
 
 # 予測
@@ -34,16 +32,16 @@ def predict(x_):
     return (y_pred, score)
 
 
-y_pred = predict(x_test.head(1))
+y_pred = predict(x_train.head(1))
 
-print('タイトル', test.title.head(1).values)
+print('タイトル', train.title.head(1).values)
 print('予測ラベル', y_pred[0])
-print('正解', y_test[0])
+print('正解', y_train[0])
 print('予測確率', y_pred[1])
 
 '''
-タイトル ['UK Stocks Rise to Two-Month High as Barclays Gains on Job Cuts']
+タイトル ["UPDATE 7-France's Iliad challenges Sprint for control of T-Mobile"]
 予測ラベル [1]
-正解 0
-予測確率 [[0.23157012 0.36831571 0.36275884 0.03735533]]
+正解 1
+予測確率 [[0.18940928 0.71075679 0.02824979 0.07158414]]
 '''
